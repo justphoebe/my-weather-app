5b21ab94217dfcbc2cbe16b9f99f1a4b8f56a094
justphoebe <phoebeholford@gmail.com>
2021-08-19 22:26:16 +0100

    Made am forecast a repeating array

--- src/index.js
+++ src/index.js
@@ -18,6 +18,43 @@ let apiUrl = `${apiStart}q=${currentcity.value}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(editHeading);
 
 }
+
+function displayForecast () { 
+let forecastamElement = document.querySelector("#forecastam");
+let forecastamHtml =  `    <div class="row am">
+      <div class="col-sm">
+        <p>
+        <div class="time">
+          am
+        </div>
+
+        </p>
+      </div>
+     
+    `;
+let days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+
+days.forEach(function (day) {
+forecastamHtml = forecastamHtml + `<div class="col-sm">
+        <p>
+        <div class="symbol">
+          <img src="images\iconfinder_weather-30_1530365.png" class="symbola" />
+        </div>
+        <div class="date">
+          ${day}
+        </div>
+        <div class="forecast">
+          10° 1°
+        </div>
+
+        </p>
+
+`;
+
+forecastamHtml = forecastamHtml + `</div>`;
+forecastamElement.innerHTML = forecastamHtml;
+});}
+
 function editHeading (response){
 console.log (response);
 console.log (response.data.name);
@@ -86,3 +123,5 @@ currentlocation.addEventListener ("click", displayCurrentcity);
 let celsiustemperature = null;
 
 displayCurrentcity();
+displayForecast ();
+
